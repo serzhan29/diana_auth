2119c5a4cc67d797fc67e43ec23370ad116b72a7
serzhan29 <133089140+serzhan29@users.noreply.github.com>
2024-05-09 22:38:20 +0500

    second  commit

--- actors/utils.py
+++ actors/utils.py
@@ -1,4 +1,6 @@
-menu = [{'title': "О сайте", 'url_name': 'about'},
+menu = [
+        {'title': "Главная страница", 'url_name': 'home'},
+        {'title': "О сайте", 'url_name': 'about'},
         {'title': "Добавить статью", 'url_name': 'add_page'},
         {'title': "Обратная связь", 'url_name': 'contact'},
         ]
